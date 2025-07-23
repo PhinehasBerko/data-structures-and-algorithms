62e03927621b110ac9259bd281c84a78e5cd11cb
ZEALOT <phinehasdscience@gmail.com>
2025-07-23 12:35:33 +0000

    chore: implement recursive functions and how it works

---
+++ dsa-index.js
@@ -0,0 +1,85 @@
+let scope = "global";
+
+function showScope() {
+    scope = "local"
+    return scope
+}
+
+console.log("Outside function, scope is: ",scope)
+
+console.log("scope variable from function: ",showScope())
+
+// Recursive functions
+
+// Recursive functions are functions that calls itself until it reaches a condition (
+// base case) that stops the recursion.
+
+// the factorial function
+
+
+// Here:
+// Recursive step: factorial(n - 1)
+
+// Base case: when n is 0 or 1
+
+function factorial(number) {
+    if (number === 0 || number === 1) return number
+    
+    return number * factorial(number-1);    
+
+}
+
+
+let number = 5;
+factorial(number);
+console.log(`Factorial of ${number} is ${factorial(number)}`);
+
+// Steps to Create a Recursive Function
+// 1. Identify the problem’s recursive nature
+
+// Ask yourself: “Can this problem be defined in terms of smaller versions of itself?”
+
+//  Example:
+
+// Factorial: n! = n × (n-1)!
+
+// Fibonacci: fib(n) = fib(n-1) + fib(n-2)
+
+// Traversing folders: "look inside folder → repeat for subfolders".
+
+// 2. Define the base case (stopping condition)
+
+// The base case tells the recursion when to stop.
+
+// Without it → infinite recursion → stack overflow.
+
+// Example:
+
+// Factorial: when n === 1 (or n === 0), return 1.
+
+// 3. Define the recursive case
+
+// Express the solution in terms of a smaller input to the same function.
+
+// Example:
+
+// Factorial: n * factorial(n - 1)
+
+// Fibonacci: fib(n - 1) + fib(n - 2)
+
+// 4. Make progress toward the base case
+
+// Ensure the input shrinks on every recursive call.
+
+// Example: factorial uses n - 1, so eventually it reaches 1.
+
+// 5. Test with small inputs
+
+// Always test with small values to verify:
+
+// The base case works.
+
+// The recursion actually reduces toward it.
+
+// Example: test factorial(1), factorial(2), factorial(3).
+
